be3040d0466580987ddad9d8192ce03a6fa15772
ArmanKadian-sys <armankadian11@gmail.com>
2026-05-12 21:57:17 +1000

    Update SimpleBar.jsx
    simple bar tailwind updated

--- src/components/SimpleBar.jsx
+++ src/components/SimpleBar.jsx
@@ -1,7 +1,7 @@
 const SimpleBar = () => {
   return (
     <>
-      <div className="min-h-screen bg-black flex items-center justify-center">
+      <div className=" bg-black flex items-center justify-center">
         <h1 className="text-white text-4xl font-bold">
           ECommerce Website
         </h1>
